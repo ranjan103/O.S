192e3851e624a384406935786f616898b88eb27f
ranjan103 <42897204+ranjan103@users.noreply.github.com>
2019-01-23 14:53:16 +0530

    Add files via upload

---
+++ System Calls/alarm.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+#include  <signal.h>
+
+
+void  ALARMhandler(int sig)
+{
+  signal(SIGALRM, SIG_IGN);          
+  printf("Hello\n");
+  signal(SIGALRM, ALARMhandler);     
+}
+
+int main(int argc, char *argv[])
+{
+  signal(SIGALRM, ALARMhandler);
+  alarm(2);   
+  signal(SIGALRM, ALARMhandler);                 
+}
---
+++ System Calls/date.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(void){
+	printf("I'm in date.c\n");
+}
